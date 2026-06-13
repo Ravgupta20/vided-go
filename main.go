a7962bc2efbb635b6b6a72566b2e7caaadda294a
Ravi <ravgupta20@gmail.com>
2026-06-12 23:33:38 -0400

    added basic go lang cli implementation

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
-import package main
+package main
 
-import (	
-
-
-
-	"fmt"	"github.com/urfave/cli/v2"
-	"os"
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
 )
 
 func main() {
-	
+	var inputVid string
+	// Equivalent to: ffmpeg -i input.mp4 -c:v libx264 -crf 23 output.mp4
+	cmd := exec.Command("ffmpeg",
+		"-version",
+	)
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	cmd.Stdout = &stdout // Captures the version info
+	cmd.Stderr = &stderr // Captures any errors
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("FFmpeg failed: %v\nError details: %s\n", err, stderr.String())
+		return
+
+	}
+
+	// Print the captured version text
+	fmt.Println(stdout.String())
+	// 	"-i", "input.mp4",
+	// 	"-c:v", "libx264",
+	// 	"-crf", "23",
+	// 	"output.mp4",
+	// )
+
 }
