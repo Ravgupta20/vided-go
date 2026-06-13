97c1d64d098da1a2beba2d96c543349faacf7e6a
Ravi <ravgupta20@gmail.com>
2026-06-13 10:06:01 -0400

    added simple audio file

--- main.go
+++ main.go
@@ -4,13 +4,27 @@ import (
 	"bytes"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
+type InputVid struct {
+	filename string
+}
+
+func (i *InputVid) Clean() {
+	i.filename = strings.ReplaceAll(i.filename, " ", "")
+}
 func main() {
-	var inputVid string
+	var outputVid string
+	var inputVid InputVid
+	inputVid.filename = "input.mov"
+	inputVid.Clean()
+	outputVid = "test_output.mp4"
 	// Equivalent to: ffmpeg -i input.mp4 -c:v libx264 -crf 23 output.mp4
 	cmd := exec.Command("ffmpeg",
-		"-version",
+		// "-version",
+		"-i", inputVid.filename,
+		"-to", "00:00:3", "-c", "copy", outputVid,
 	)
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
@@ -27,10 +41,21 @@ func main() {
 
 	// Print the captured version text
 	fmt.Println(stdout.String())
-	// 	"-i", "input.mp4",
 	// 	"-c:v", "libx264",
 	// 	"-crf", "23",
 	// 	"output.mp4",
 	// )
 
+}
+
+// Extract Audio Only Slice
+func extractAudio(input, outputAudio string) *exec.Cmd {
+	args := []string{
+		"-y",
+		"-i", input,
+		"-vn",            // Disable video recording
+		"-acodec", "mp3", // Encode audio to MP3
+		outputAudio,
+	}
+	return exec.Command("ffmpeg", args...)
 }
